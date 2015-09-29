f5c01537d60725c21504a51af006d0540bbab71e
rolandoldengarm <r_oldengarm@hotmail.com>
2015-09-29 13:55:53 +1300

    nuget update

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ gulp.task('minify', function() {
     gulp.src('src/js/*.js')
         .pipe(concat('fabricui-directives.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./content/'))
         .pipe(gulp.dest('./dist/'));
 });
 
