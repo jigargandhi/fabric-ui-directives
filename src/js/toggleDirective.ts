9b945ee53b106bcab03a4b7f8af33ea7f5abf519
rolandoldengarm <r_oldengarm@hotmail.com>
2015-09-29 13:46:42 +1300

    nuget version update

--- src/js/toggleDirective.ts
+++ src/js/toggleDirective.ts
@@ -8,7 +8,7 @@ var appModule = angular.module("fabricuiDirectives", []);
 class ToggleDirective implements ng.IDirective {
     
     template = '<div class="ms-Toggle">' +
-                '<span class="ms-Toggle-description">{{desc}}</span>' +
+                 '<span class="ms-Toggle-description">{{desc}}</span>' +
                 '<input type="checkbox" id="{{uniqueId}}" class="ms-Toggle-input" ng-model="toggled" />' +
                 '<label for="{{uniqueId}}" class="ms-Toggle-field">' +
                     '<span class="ms-Label ms-Label--off">{{labelOff}}</span>' +
